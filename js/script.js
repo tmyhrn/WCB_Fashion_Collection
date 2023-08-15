01afeb364bd12e382189a4d7e2c56f4fb0f9bdd1
Hirano Tomoya <ht.tomoya0605@gmail.com>
2023-08-15 07:29:54 +0000

    7-9終了

--- js/script.js
+++ js/script.js
@@ -59,3 +59,41 @@ thumbImages.forEach((thumbImages)=>{
     mainImage.animate({opacity: [0, 1]}, 500)
   });
 });
+
+//スライドメニュー
+const menuOpen = document.querySelector('#menu-open');
+const menuClose = document.querySelector('#menu-close');
+const menuPanel = document.querySelector('#menu-panel');
+const menuItems = document.querySelectorAll('#menu-panel li')
+const menuOptions = {
+  duration: 1400,
+  easing: 'ease',
+  fill: 'forwards',
+};
+
+//メニューを開く
+menuOpen.addEventListener('click', () => {
+  menuPanel.animate({translate: ['100vw', 0]}, menuOptions);
+  menuItems.forEach((menuItem, index) => {
+    menuItem.animate(
+      {
+        opacity: [0, 1],
+        translate: ['2rem', 0],
+      },
+      {
+        duration: 2400,
+        delay: 300 * index,
+        easing: 'ease',
+        fill: 'forwards',
+      }
+      );
+  });
+})
+
+//メニューを閉じる
+menuClose.addEventListener('click', () => {
+  menuPanel.animate({translate: [0, '100vw']}, menuOptions);
+  menuItems.forEach((menuItem) => {
+    menuItem.animate({opacity: [1, 0]}, menuOptions);
+  });
+})
