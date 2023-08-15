1b46822e1b2cc5ba8824acf835b64fe7c75e5a49
Hirano Tomoya <ht.tomoya0605@gmail.com>
2023-08-15 02:49:12 +0000

    7-4終了

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
 const loadingAreaGrey = document.querySelector('#loading');
 const loadingAreaGreen = document.querySelector('#loading-screen');
+const loadingText = document.querySelector('#loading p');
 
 window.addEventListener('load', () => {
-  loadingAreaGrey.animate (
+  // ローディング中（グレースクリーン）
+  loadingAreaGrey.animate(
     {
       opacity: [1, 0],
-      visibility: 'hidden'
+      visibility: 'hidden',
     },
     {
       duration: 2000,
@@ -13,17 +15,37 @@ window.addEventListener('load', () => {
       easing: 'ease',
       fill: 'forwards',
     }
-    );
-    
-    loadingAreaGreen.animate(
+  );
+
+  // ローディング中（薄緑スクリーン）
+  loadingAreaGreen.animate(
+    {
+      translate: ['0 100vh', '0 0', '0 -100vh']
+    },
+    {
+      duration: 2000,
+      delay: 800,
+      easing: 'ease',
+      fill: 'forwards',
+    }
+  );  
+
+  // ローディング中テキスト
+  loadingText.animate(
+    [
       {
-        translate: ['0 100vh', '0 0', '0 -100vh']
+        opacity: 1,
+        offset: .8  //80%
       },
       {
-        duration: 2000,
-        delay: 800,
-        easing: 'ease',
-        fill: 'forwards',
-      }
-      );
+        opacity: 0,
+        offset: 1  //100%
+      },
+    ], 
+    {
+      duration: 1200,
+      easing: 'ease',
+      fill: 'forwards',
+    }
+  );
 });
