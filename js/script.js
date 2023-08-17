acb68890f1a9efa319fad36a66603a1c96d81051
Hirano Tomoya <ht.tomoya0605@gmail.com>
2023-08-17 01:52:35 +0000

    7-12終了

--- js/script.js
+++ js/script.js
@@ -98,3 +98,33 @@ menuClose.addEventListener('click', () => {
     menuItem.animate({opacity: [1, 0]}, menuOptions);
   });
 });
+
+//スクロールで要素を表示
+
+const animateFade = (entries, obs) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.animate(
+        {
+          opacity: [0, 1],
+          filter: ['blur(.4rem)', 'blur(0)'],
+          translate: ['0 4rem', 0]
+        },
+        {
+          duration: 2000,
+          easing: 'ease',
+          fill: 'forwards',
+        }
+      );
+      obs.unobserve(entry.target);
+    }
+  });
+};
+
+//監視設定
+const  fadeObserver = new IntersectionObserver(animateFade);
+
+const fadeElements = document.querySelectorAll('.fadein');
+fadeElements.forEach((fadeElement) => {
+  fadeObserver.observe(fadeElement);
+});
